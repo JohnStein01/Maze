99f699c8a4936528e350451b123a58f9cc9e8d80
JohnStein01 <andro.john01@gmail.com>
2018-05-03 20:41:19 +0300

    Maze

---
+++ Assets/Pathfinding/Scene/Scripts/Aim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour
+{	
+	public GameObject Target;
+	public GameObject Bullet;
+
+	void Start(){
+		StartCoroutine(WaitAndShoot());
+	}
+
+	private IEnumerator WaitAndShoot(){		
+		yield return new WaitForSeconds(21);
+		transform.rotation = Quaternion.LookRotation (Target.transform.position);
+		yield return new WaitForSeconds (2);
+		Vector3 Deneme = Bullet.transform.position - Target.transform.position;
+		Deneme.y = 0;
+		Deneme.z = Deneme.z + 1.5f;
+		Bullet.GetComponent<Rigidbody> ().velocity = Deneme;
+	}
+}
---
+++ Assets/Pathfinding/Scene/Scripts/Hit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour {
+	public GameObject HitText;
+	// Use this for initialization
+	void Start () {
+		HitText.gameObject.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.name == "Bullet") {			
+			Destroy (c.gameObject);
+			HitText.gameObject.SetActive (true);
+		}		
+	}
+}
